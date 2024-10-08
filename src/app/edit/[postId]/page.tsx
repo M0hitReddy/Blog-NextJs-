f4b6be13fb6f64eb0f5bccb252be34f36bb177be
M0hitReddy <mohitreddydadireddy@gmail.com>
2024-10-09 00:57:37 +0530

    fix: only author can edit post

--- src/app/edit/[postId]/page.tsx
+++ src/app/edit/[postId]/page.tsx
@@ -9,15 +9,18 @@ import { useEditor } from "@tiptap/react";
 import Underline from "@tiptap/extension-underline";
 import CodeBlock from "@tiptap/extension-code-block";
 import Image from "@tiptap/extension-image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import axios from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 import { Post } from "@/types/schemas";
 import { Loader2 } from "lucide-react";
+import { useSession } from "next-auth/react";
 // import { useToast } from "@/hooks/use-toast";
 
 export default function Edit() {
   const { setEditor, setTitle } = useEditorContext();
+  const router = useRouter();
+  const session =useSession();
   const path = usePathname();
   const postId = path.split("/").pop();
   const [post, setPost] = useState<Post | null>(null);
@@ -55,13 +58,19 @@ export default function Edit() {
 
   useEffect(() => {
     // const saveDraft = searchParams.get("saveDraft");
-
+    if(session.status === "loading") return;
     const fetchPost = async () => {
       try {
         const response = await axios.get<ApiResponse<Post>>(
           `/api/post?postId=${postId}`
         );
         const post = response.data.data;
+        console.log(post.author, session);
+        if(post.author?.id !== session.data?.user.id) {
+          
+          router.push(`/post/${postId}`);
+          return;
+        }
         setPost(post);
         // setEditor(post.content);
       } catch (error) {
@@ -78,7 +87,7 @@ export default function Edit() {
     //     description: "Your blog post has been successfully saved as a draft.",
     //     variant: "default",
     //   });
-  }, [path, postId]);
+  }, [path, postId, session.status, router, session]);
 
   useEffect(() => {
     if (!post) return;
@@ -87,7 +96,7 @@ export default function Edit() {
     newEditor && newEditor.commands.setContent(postContent || "");
     setTitle(post.title);
     setEditor(newEditor);
-  }, [post, newEditor, setEditor, setTitle]);
+  }, [post, newEditor, setEditor, setTitle, router, session]);
 
   useEffect(() => {
     if (path.startsWith("/edit")) {
